5d338536e0eb7f47e2523494103ecbd54ca875e2
nx101 <revelight@gmail.com>
2019-02-03 13:55:08 +0200

    dependency injection - demo improved

--- src/main/java/guru/springframework/services/PrimarySpanishGreetingService.java
+++ src/main/java/guru/springframework/services/PrimarySpanishGreetingService.java
@@ -1,15 +1,17 @@
 package guru.springframework.services;
 
+
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile("es")
 @Primary
-@Profile("en")
-public class PrimaryGreetingService implements GreetingService {
+public class PrimarySpanishGreetingService implements GreetingService {
+
     @Override
     public String sayGreeting() {
-        return "Hello - PrimaryGreetingService";
+        return "~**&Hola&**~ - PrimarySpanishGreetingService";
     }
 }
