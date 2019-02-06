6b78d62dd0c8949fe61166a280f9c6d8a1708444
nx101 <revelight@gmail.com>
2019-02-06 20:00:11 +0200

    switched PrimaryGreeting services from class Annotations to @Configuration class wiring. Added a GreetingService factory for the primary en, es, and de variants.

---
+++ src/main/java/guru/springframework/config/GreetingClassConfig.java
@@ -0,0 +1,42 @@
+package guru.springframework.config;
+
+import guru.springframework.services.GreetingRepository;
+import guru.springframework.services.GreetingService;
+import guru.springframework.services.GreetingServiceFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class GreetingClassConfig {
+
+    @Bean
+    // get a factory instance
+    GreetingServiceFactory greetingServiceFactory(GreetingRepository repository){
+        return new GreetingServiceFactory(repository);
+    }
+
+    @Bean
+    @Primary
+    @Profile({"default", "en"})
+    GreetingService primaryGreetingServiceEn(GreetingServiceFactory greetingServiceFactory){
+        return greetingServiceFactory.createGreetingService("en");
+    }
+
+    @Bean
+    @Primary
+    @Profile("es")
+    GreetingService primaryGreetingServiceEs(GreetingServiceFactory greetingServiceFactory){
+        return greetingServiceFactory.createGreetingService("es");
+    }
+
+    @Bean
+    @Primary
+    @Profile("de")
+    GreetingService primaryGreetingServiceDe(GreetingServiceFactory greetingServiceFactory){
+        return greetingServiceFactory.createGreetingService("de");
+    }
+
+
+}
