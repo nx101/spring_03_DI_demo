176735fd8f50debc90c515c7dab3b2546ba7efaf
nx101 <revelight@gmail.com>
2019-02-06 19:08:09 +0200

    add @ComponentScan demo with external package

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -7,8 +7,16 @@ import guru.springframework.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
+// Spring boot app root
 @SpringBootApplication
+
+// If we want to override the default spring boot Component Scan,
+// of only root and below it (root == package guru.springframework),
+// We must explicitly specify all the base packages (base : it and below).
+// Like basePackages, we have more options to specify by classes and more
+@ComponentScan(basePackages = {"guru.springframework", "guru.external.somepackage"})
 public class DiDemoApplication {
 
 	public static void main(String[] args) {
